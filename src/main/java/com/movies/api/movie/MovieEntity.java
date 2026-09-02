19e7346b5c82ac6b92a13fee08efe0da1a7d3f6b
Iulian Timofei <iuliantim21@gmail.com>
2026-09-02 21:09:01 +0200

    feat. add Movie entity with basic fields

---
+++ src/main/java/com/movies/api/movie/MovieEntity.java
@@ -0,0 +1,60 @@
+package com.movies.api.movie;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
+@Entity
+@Table(name = "movies")
+public class MovieEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_movie")
+    private Long id;
+
+    private String title;
+    private String synopsis;
+    private Integer duration;
+
+    public MovieEntity() {
+    }
+
+    public MovieEntity(Long id, String title, String synopsis, Integer duration) {
+        this.id = id;
+        this.title = title;
+        this.synopsis = synopsis;
+        this.duration = duration;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+}
